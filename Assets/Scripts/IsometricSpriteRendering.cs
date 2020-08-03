e18dbe692eace572596cb053b352f676bb82742b
Efe Tas <etas@purdue.edu>
2020-08-04 00:23:48 +0300

    first commit

---
+++ Assets/Scripts/IsometricSpriteRendering.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsometricSpriteRendering : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Renderer>().sortingOrder = (int)(transform.position.y * -10);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D theRB;
+    public float moveSpeed;
+    public Animator myAnim;
+    public static PlayerController instance;
+    public string areaTransitionName;
+    // Start is called before the first frame update
+    // Use this for initialization
+    void Start()
+    {
+        if(instance == null){
+            instance = this;
+        }else{
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;
+        myAnim.SetFloat("moveY", theRB.velocity.y);
+        myAnim.SetFloat("moveX", theRB.velocity.x);
+
+        if(Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1){
+            myAnim.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
+            myAnim.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+        }
+    }
+}
